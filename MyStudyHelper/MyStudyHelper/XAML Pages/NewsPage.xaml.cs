11b8b32ddfbf002fb89e820833fd6423db17f7de
Kawhina Hohepa <27037080@student.toiohomai.ac.nz>
2020-03-22 12:29:46 +1300

    Created 3 pages, home, posts and news pages, started a bit of frontend design however left it mostly empty due to these pages requiring connection to database and api setup before continuing work on frontend. Connected the 3 via MainPage.

---
+++ MyStudyHelper/MyStudyHelper/XAML Pages/NewsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyStudyHelper.XAML_Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewsPage : ContentPage
+    {
+        public NewsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
